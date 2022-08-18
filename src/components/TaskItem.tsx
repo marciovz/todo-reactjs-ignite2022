9127fd237f5abdd23600e16aad039737fe5ce75c
marciovz <marciovz@yahoo.com.br>
2022-08-17 22:35:53 -0300

    implementado o componente TaskItem

---
+++ src/components/TaskItem.tsx
@@ -0,0 +1,43 @@
+import { Trash} from 'phosphor-react';
+
+import { CheckBox } from './CheckBox';
+
+import styles from './TaskItem.module.css';
+
+interface ITask {
+  id: string;
+  title: string;
+  done: boolean;
+}
+
+interface ITaskItemProps {
+  task: ITask;
+  onToggleTask: (id: string) => void;
+  onRemoveTask: (id: string) => void;
+}
+
+export function TaskItem({task, onToggleTask, onRemoveTask }: ITaskItemProps) {
+
+  const handleChangeCheckBox = () => {
+    onToggleTask(task.id);
+  }
+
+  const handleClickButton = () => {
+    onRemoveTask(task.id);
+  }
+
+  return(
+    <div className={styles.task} key={task.id}>
+      <CheckBox checked={task.done} onChange={handleChangeCheckBox} />
+      <p className={task.done ? styles.taskDone : ''}>{task.title}</p>
+      <button 
+        type="button"
+        className={styles.trashButton}
+        onClick={handleClickButton}
+      >
+        <Trash />
+      </button>
+  </div>
+
+  )
+}
